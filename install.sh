1702ca751c9bea6625998ea14501abb897c81b2e
root <root@localhost.localdomain>
2019-05-07 22:03:55 +0530

    added files

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ wget https://bootstrap.pypa.io/get-pip.py
 pypy get-pip.py
 pypy -m pip install requests
 pypy -m pip install argparse
-sudo cp pybuster /usr/bin/pybuster
+sudo cp pybuster.py /usr/bin/pybuster
 sudo chmod 755 /usr/bin/pybuster
 rm get-pip.py
 echo "[+] Installed pybuster"
